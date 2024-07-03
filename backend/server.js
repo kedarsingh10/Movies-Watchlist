f0eeed416c2c2e838ddb29157f0630adfdba43a1
kedarsingh10 <kedarchhetri10@gmail.com>
2024-07-04 02:56:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,10 @@ app.use("/api/movies", moviesRoutes);
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
 );
 
 // const __dirname = path.resolve();
